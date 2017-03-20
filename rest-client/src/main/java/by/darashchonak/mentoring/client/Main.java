7766876e92e15bd97e5f102e3127c913c9d3b5ae
baltkrievs <6406640@gmail.com>
2017-03-20 19:11:55 +0300

    draft implenetation

--- rest-client/src/main/java/by/darashchonak/mentoring/client/Main.java
+++ rest-client/src/main/java/by/darashchonak/mentoring/client/Main.java
@@ -12,6 +12,7 @@ import org.glassfish.jersey.client.ClientConfig;
 
 import com.google.gson.Gson;
 
+import by.darashchonak.mentoring.service.JaxParamBean;
 import by.darashchonak.mentoring.service.User;
 
 public class Main {
@@ -23,26 +24,68 @@ public class Main {
 
         WebTarget webTarget = client.target("http://localhost:9090/rest").path("user");
 
-        User user = new User();
-        user.setName("Meganame");
-        user.setEmail("asdasds@emaik.org");
-        user.setAvatar("asad");
+        User user1 = new User();
+        user1.setName("John Travolta");
+        user1.setEmail("John@email.org");
+        user1.setAvatar("asad");
+
+        User user2 = new User();
+        user2.setName("Uma Thurman");
+        user2.setEmail("Uma@email.org");
+        user2.setAvatar("asad");
 
         Gson gson = new Gson();
-        String json = gson.toJson(user);
+        String json1 = gson.toJson(user1);
+        String json2 = gson.toJson(user2);
 
-        System.out.println(json);
-
+        // Create two users
         Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.put(Entity.entity(json1, MediaType.APPLICATION_JSON), Response.class);
+        System.out.println(response.getStatus() + " " + response.getLocation().toString());
+        response = invocationBuilder.put(Entity.entity(json2, MediaType.APPLICATION_JSON), Response.class);
+        System.out.println(response.getStatus() + " " + response.getLocation().toString());
 
-        Response response = invocationBuilder.put(Entity.entity(json, MediaType.APPLICATION_JSON), Response.class);
+        // Get user by Id
+        webTarget = client.target("http://localhost:9090/rest").path("user").path("2");
+        invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+        response = invocationBuilder.get();
 
+        User umaThurman = response.readEntity(User.class);
+
+        // Print users' email
+        System.out.println(umaThurman.getEmail());
+
+        // Update email
+        JaxParamBean params = new JaxParamBean();
+        params.setEmail("uma_therman1970@gmail.com");
+        webTarget = client.target("http://localhost:9090/rest").path("user").path("2").path("email");
+        invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+        response = invocationBuilder.post(Entity.entity(gson.toJson(params), MediaType.APPLICATION_JSON),
+                Response.class);
         System.out.println(response.getStatus());
 
+        // Get the same user again
         webTarget = client.target("http://localhost:9090/rest").path("user").path("2");
+        invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+        response = invocationBuilder.get();
+        User umaThurmanUpdated = response.readEntity(User.class);
+
+        // Email has been changed
+        System.out.println(umaThurmanUpdated.getEmail());
+
+        // Get another user
+        webTarget = client.target("http://localhost:9090/rest").path("user").path("1");
+        response = invocationBuilder.get();
+        User johnTravolta = response.readEntity(User.class);
+        System.out.println(johnTravolta.getName());
+
+        // Delete user
         invocationBuilder = webTarget.request();
         response = invocationBuilder.delete();
+        System.out.println(response.getStatus());
 
+        // Get user again - no such user status 204
+        response = invocationBuilder.get();
         System.out.println(response.getStatus());
 
     }
